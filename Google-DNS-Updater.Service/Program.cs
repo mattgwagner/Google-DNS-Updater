6420a486a15f6a0afd39e20327a6f468aa0260ea
Matt Wagner <mattgwagner@gmail.com>
2015-02-01 21:15:23 -0500

    Initial work done

--- Google-DNS-Updater.Service/Program.cs
+++ Google-DNS-Updater.Service/Program.cs
@@ -1,23 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Common.Logging;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Configuration;
+using Topshelf;
 
 namespace Google_DNS_Updater.Service
 {
     internal class Program
     {
-        private static String Hostname = "";
+        public static String Hostname = ConfigurationManager.AppSettings["Hostname"];
 
-        private static String Username = "";
+        public static String Username = ConfigurationManager.AppSettings["Username"];
 
-        private static String Password = "";
+        public static String Password = ConfigurationManager.AppSettings["Password"];
 
         private static int UpdateIntervalInMinutes = 720;
 
         private static void Main(string[] args)
         {
+            HostFactory.Run(x =>
+            {
+                x.Service<TaskService>();
+
+                x.SetDescription("Google Dynamic DNS Updater");
+                x.SetDisplayName("Google - Dynamic DNS Updater");
+                x.SetServiceName("Google-Dynamic-DNS-Updater");
+
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
+                x.EnableServiceRecovery(s => s.RestartService(1)); // restart after 1 minute
+            });
+        }
+
+        private class TaskService : ServiceControl
+        {
+            private static readonly ILog Log = LogManager.GetLogger<TaskService>();
+
+            private readonly IScheduler scheduler;
+
+            public TaskService()
+            {
+                this.scheduler = new StdSchedulerFactory().GetScheduler();
+            }
+
+            public bool Start(HostControl hostControl)
+            {
+                Log.Info("Starting Google Dynamic DNS Updater Servcice");
+
+                IJobDetail job =
+                    JobBuilder
+                    .Create()
+                    .OfType<UpdateJob>()
+                    .Build();
+
+                ITrigger trigger =
+                    TriggerBuilder
+                    .Create()
+                    .ForJob(job)
+                    .StartNow()
+                    .WithSimpleSchedule(x =>
+                        x
+                        .WithIntervalInMinutes(Program.UpdateIntervalInMinutes)
+                        .RepeatForever()
+                    )
+                    .Build();
+
+                this.scheduler.ScheduleJob(job, trigger);
+
+                this.scheduler.Start();
+
+                return true;
+            }
+
+            public bool Stop(HostControl hostControl)
+            {
+                Log.Info("Stopping Google DNS Updater Service");
+
+                this.scheduler.Shutdown(waitForJobsToComplete: false);
+
+                return true;
+            }
         }
     }
 }
---
+++ Google-DNS-Updater.Service/UpdateJob.cs
@@ -0,0 +1,29 @@
+﻿using Common.Logging;
+using Quartz;
+using System;
+using System.Net.Http;
+
+namespace Google_DNS_Updater.Service
+{
+    public class UpdateJob : IJob
+    {
+        private readonly ILog Log = LogManager.GetLogger<UpdateJob>();
+
+        public async void Execute(IJobExecutionContext context)
+        {
+            Log.InfoFormat("Running update for hostname {0}", Program.Hostname);
+
+            String url = String.Format("https://{0}:{1}@domains.google.com/nic/update?hostname={2}",
+                Program.Username,
+                Program.Password,
+                Program.Hostname);
+
+            using (HttpClient client = new HttpClient())
+            {
+                await client.GetAsync(url);
+            }
+
+            Log.Info("Update completed.");
+        }
+    }
+}
