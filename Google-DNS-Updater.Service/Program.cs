4ce0c366a474eda6472e0000c2107029e809eab5
Matt Wagner <mattgwagner@gmail.com>
2015-02-01 20:41:41 -0500

    Initial code and project setup drop

---
+++ Google-DNS-Updater.Service/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google_DNS_Updater.Service
+{
+    internal class Program
+    {
+        private static String Hostname = "";
+
+        private static String Username = "";
+
+        private static String Password = "";
+
+        private static int UpdateIntervalInMinutes = 720;
+
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Google-DNS-Updater
+====================
+
+A small Windows service to retrieve and update dynamic DNS records via the dyndns protocol for Google Domains.
+
+Installing
+====================
+
+TODO
