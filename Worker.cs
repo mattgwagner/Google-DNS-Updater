e5cf368cbc4471c0eb8009746fcdc69729f9be41
Matt Wagner <mattgwagner@gmail.com>
2020-02-18 21:58:27 -0500

    Move settings to file

--- Worker.cs
+++ Worker.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -10,19 +10,35 @@ namespace Google_DNS_Updater
 {
     public class Worker : BackgroundService
     {
+        private static readonly HttpClient http = new HttpClient{};
+
         private readonly ILogger<Worker> _logger;
 
-        public Worker(ILogger<Worker> logger)
+        private readonly IConfiguration configuration;
+
+        private string Username => configuration.GetValue<string>(nameof(Username));
+
+        private string Password => configuration.GetValue<string>(nameof(Password));
+
+        private string Hostname => configuration.GetValue<string>(nameof(Hostname));
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
-            _logger = logger;
+            this._logger = logger;
+            this.configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Starting!");
+
+                var json = await http.GetStringAsync($"https://{Username}:{Password}@domains.google.com/nic/update?hostname={Hostname}");
+
+                _logger.LogInformation("Updated Google DNS record: {json}", json);
+
+                await Task.Delay((int)TimeSpan.FromHours(4).TotalSeconds, stoppingToken);
             }
         }
     }
