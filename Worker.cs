23945b595bbeaafd67ae26fbe5019878aac1d80a
Matt Wagner <mattgwagner@gmail.com>
2020-02-18 22:16:01 -0500

    Working with auth in header

--- Worker.cs
+++ Worker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -15,28 +16,36 @@ namespace Google_DNS_Updater
         private readonly ILogger<Worker> _logger;
 
         private readonly IConfiguration configuration;
-
-        private string Username => configuration.GetValue<string>(nameof(Username));
-
-        private string Password => configuration.GetValue<string>(nameof(Password));
-
         private string Hostname => configuration.GetValue<string>(nameof(Hostname));
 
         public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             this._logger = logger;
             this.configuration = configuration;
+
+            http.DefaultRequestHeaders.Add("User-Agent", "Google-DNS-Updater");
+
+            var bytes = Encoding.ASCII.GetBytes($"{configuration.GetValue<string>("User")}:{configuration.GetValue<string>("Password")}");
+
+            http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(bytes));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Starting!");
+                _logger.LogInformation("Starting to Update {hostname}", Hostname);
 
-                var json = await http.GetStringAsync($"https://{Username}:{Password}@domains.google.com/nic/update?hostname={Hostname}");
+                try
+                {              
+                    var json = await http.GetAsync($"https://domains.google.com/nic/update?hostname={Hostname}");
 
-                _logger.LogInformation("Updated Google DNS record: {json}", json);
+                    _logger.LogInformation("Updated Google DNS record: {json}", json);                    
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex, "Error updating record!");
+                }
 
                 await Task.Delay((int)TimeSpan.FromHours(4).TotalSeconds, stoppingToken);
             }
